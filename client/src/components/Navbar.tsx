b5b5b195e5cc666471b972248673dd4cf9ac38a4
Sahith Chandra <sahithchandra@10-248-23-19.wireless.oregonstate.edu>
2025-04-05 18:30:17 -0700

    updates to navbar👅

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,11 +8,12 @@ const Navbar = (props: Props) => {
     <nav className="top-0 w-full fixed">
       <div className="mt-10 flex flex-row justify-between items-center text-black">
         <div className=" ml-15 text-black flex items-center text-7xl font-semibold">
-          <img src={logo} alt="logo" className="w-20 h-20 mt-2"></img>
+          <img src={logo} alt="logo" className="w-30 h-30 mt-2"></img>
           <h1 className="">CareConnect</h1>
         </div>
-        <button className="mr-15 flex font-semibold btn btn-outline text-xl" >Sign in </button>
+        <button className="mr-15 flex font-semibold btn btn-outline text-3xl py-4" >Sign in </button>
       </div>
+      <text id="phrase" className=" ml-47 text-black text-3xl italic" >Support, just a tap away... </text>
     </nav>
   );
 };
